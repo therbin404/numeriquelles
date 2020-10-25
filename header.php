43f161534a8a14fdd82e54f3004ab5a50f29c3b7
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-10-25 23:36:33 +0100

    added desktop version of header

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
               <a href="index.php" tilte="Vers la page d'accueil"><img src="images/logo.svg" alt="numeriqu'elles"></a>
             </div>
             <div class="menu">
-              <img src="images/menu.svg" alt="menu" id="menu">
+              <div class="desktop">
                 <ul class="firstLevel" id="firstLevel">
                   <li>
                     <a href="" tilte="Préparer Numériqu’elles">Préparer Numériqu’elles</a>
@@ -60,12 +60,56 @@
                     </ul>
                   </li>
                 </ul>
+              </div>
+              <div class="responsive">
+                <img src="images/menu.svg" alt="menu" id="menu">
+                <ul class="firstLevel" id="firstLevel">
+                  <li>
+                    <a href="" tilte="Préparer Numériqu’elles">Préparer Numériqu’elles</a>
+                  </li>
+                  <li>
+                    <a href="" tilte="Pour aller plus loin">Pour aller plus loin</a>
+                  </li>
+                  <li id="menuSecondLevel">
+                    <a href="" tilte="Banque de stage">Banque de stage</a>
+                    <ul class="secondLevel" id="secondLevel">
+                      <li>
+                        <a href="" tilte="monstage3ème" target="_blank">monstage3ème</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="monstageenligne.fr" target="_blank">monstageenligne.fr</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="monstagedetroisieme.fr" target="_blank">monstagedetroisieme.fr</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="stage3e.pasdecalais.fr" target="_blank">stage3e.pasdecalais.fr</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="" tilte="Accompagnement au stage">Accompagnement au stage</a>
+                  </li>
+                  <li id="menuThirdLevel">
+                    <a href="" tilte="Les évenements" id="menuThirdLevel">Les évenements</a>
+                    <ul class="thirdLevel" id="thirdLevel">
+                      <li>
+                        <a href="" tilte="Lille">Lille</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="Maubeuge">Maubeuge</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="Boulogne">Boulogne</a>
+                      </li>
+                      <li>
+                        <a href="" tilte="Lens">Lens</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <script type="text/javascript" src="js/menu.js"></script>
+            <script type="text/javascript" src="script/menu.js"></script>
         </nav>
     </header>
-    <div class="headerIndex">
-      <img src="images/mainheader.jpg" alt="image d'en-tête">
-    </div>
-</body>
-</html>
--- script/menu.js
+++ script/menu.js
@@ -1,7 +1,25 @@
-document.querySelector('#menu').addEventListener('click', function (e) {
-    if (document.querySelector('#blocMenu').style.display == 'none') {
-        document.querySelector('#blocMenu').style.display = 'block';
+document.querySelector('#menu').addEventListener('click', function (e) { //Show first menu
+    if (document.querySelector('#firstLevel').style.display == 'block') {
+        document.querySelector('#firstLevel').style.display = 'none';
     } else {
-        document.querySelector('#blocMenu').style.display = 'none';
+        document.querySelector('#firstLevel').style.display = 'block';
+    }
+})
+
+document.querySelector('#menuSecondLevel').addEventListener('click', function (e) { //Show second menu (stages)
+    e.preventDefault();
+    if (document.querySelector('#secondLevel').style.display == 'block') {
+        document.querySelector('#secondLevel').style.display = 'none';
+    } else {
+        document.querySelector('#secondLevel').style.display = 'block';
+    }
+})
+
+document.querySelector('#menuThirdLevel').addEventListener('click', function (e) { //Show third menu (events)
+    e.preventDefault();
+    if (document.querySelector('#thirdLevel').style.display == 'block') {
+        document.querySelector('#thirdLevel').style.display = 'none';
+    } else {
+        document.querySelector('#thirdLevel').style.display = 'block';
     }
 })
