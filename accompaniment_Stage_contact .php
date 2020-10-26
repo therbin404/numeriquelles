baf8950e99dbf07d58571933865ef348609c242e
laurent <lauludev@hotmail.com>
2020-10-26 17:00:58 +0100

    initial commit

---
+++ accompaniment_Stage_contact .php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"type="text/css" href="style_accompaniment_stage_contact.css">
+    <title>contacter l'acompagnement aux stages</title>
+</head>
+<body>
+    <?php include 'header.php'?>
+    <div class="toppage"></div>
+    <div class="titleDecoration">
+        <div class="title">
+            <div class="bar"></div>
+            <div>
+              <h1>CONTACT</h1>
+            </div>
+        </div>
+    </div>
+    <p>Dans une volonté de découvrir de plus prêt les métiers du numérique vous 
+        souhaitez ou êtes déjà en stage dans une entreprise du numérique mais
+         vous avez besoin de conseils ou une réponse à vos question . Madame 
+         DELROT Murielle sera répondre à vos interrogations . 
+    </p>
+    <div class="contactwindows">
+        <p class="tall">Murielle DELROT
+        <p>03 20 15 60 29</p>
+        <p>cee@ac-lille.fr</p>
+    </div>
+    <?php include 'footer.php' ?>
+</body>
+</html>
