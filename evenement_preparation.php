b9fefd9b5675c0ff7e3b80937ace5d3a24a66437
laurent <lauludev@hotmail.com>
2020-10-26 16:54:46 +0100

    initial commit

---
+++ evenement_preparation.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"type="text/css" href="style_evenement_preparation.css">
+    <title>contacter l'accompagnement au stages</title>
+</head>
+<body>
+   <?php include 'header.php' ?> 
+   <div class="toppage"></div>
+    <div class="title">
+        <img src="images/AnoukDesury-Numeriquelles2019-10 (1).jpg" alt="">
+        <h1>PREPARER L’EVENEMENT</h1>
+        <hr class="firstline">
+    </div>
+    <p>Pourquoi les femmes renoncent-elles aux métiers du numérique et de 
+        l’informatique ? Les filles sont-elles des « handicapées du numérique
+         et de l’informatique » ? Il est vrai que les femmes sont aujourd’hui
+          sous-représentées dans les filières du numérique et de l’informatique
+         ; pourtant cela n’a pas toujours été le cas.
+    </p>
+    <div class="imgalign">
+        <img src="images/regard.png" alt="graphique qui montre que le regard des 
+        femmes ayant participé à l'événement à changer pour 86% d'entre elle">
+        <img src="images/orientation.png" alt="graphique qui montre l'influance 
+        sur l'orientation futur des participante, la moitier d'entre elle songe à 
+        s'orienté vers le numérique">
+        <img src="images/graphiquemétier.png" alt="graphique qui montre que 98 % 
+        d'entre elle on une meillleur connaissance du métier aprés l'évenement">
+        <img src="images/Satisfaction.png" alt="graphique qui montre que la 
+        moitié des participante à trouvé l'événement bien , un quart l'a trouvé 
+        super et le quart réstant n'a pas d'avis">
+        <h2>Découvrez les métiers avant les professionnels</h2>
+    </div>
+    <img src="images/onisep.png" alt="logos de l'onisep"class="onisep">
+    <p>L’onisep Hauts de France site d’Amiens vous propose 8 séquences pédagogiques utiles pour préparer et exploiter les manifestations Numériqu’elles.
+        Destinées aux élèves de collège et/ou de lycée, ces séquences vont leur permettre d’identifier les stéréotypes liés aux métiers du numérique, de mettre en lumière des parcours de femmes dans ce secteur et d’amorcer une réflexion sur la construction de leur parcours d’orientation.
+        Ces séquences clés-en-main sont indépendantes les unes des autres et s’inscrivent dans le cadre du parcours Avenir.</p>
+    <div class="interpaged">
+    <div class="interblock"></div>
+        <hr class="secondline">
+    </div> 
+    <img src="images/imageonisep.jpg" alt="image montrant un 
+    homme avec des connéction entre ses main écarté dans le style matrix"class="img_onisep">
+    <?php include 'footer.php' ?>   
+</body>
+</html>
