ad5c6f0ad16c47f5c12c0a6c66fedc73e9d1e1d2
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-10-26 12:56:47 +0100

    added structure of event page

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
   require_once('header.html');
 
-  // if(isset($_GET['page'])) {
-  //   require_once($_GET['page'].'.html');
-  // } else {
-  //   require_once('main.html');
-  // }
+  if(isset($_GET['page'])) {
+    require_once($_GET['page'].'.html');
+  } else {
+    require_once('main.html');
+  }
 
   require_once('footer.html');
 ?>
---
+++ lille.php
@@ -0,0 +1,508 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="styleevent.css">
+      <title>Numériqu'elles - Lille</title>
+  </head>
+  <body>
+
+    <?php
+      require_once('header.html');
+    ?>
+    <!-- include a jumbotron (general classes) -->
+    <div class="event">
+      <h2>Lille</h2>
+      <p>6 novembre</p>
+    </div>
+    <div class="parts">
+      <a href="#middleSchool" title="Partie collège">Partie collège</a>
+      <a href="#highSchool" title="Partie lycée">Partie lycée</a>
+    </div>
+    <!-- include an img with the green line (general classes) VIRTUAL ROOM-->
+    <section class="middleSchool" id="middleSchool">
+      <h3>Collège</h3>
+      <p>Début à 9h</p>
+      <!-- include a youtube video-->
+      <!-- include an img with the green line (general classes) ROADMAP-->
+      <p>Les ateliers partenaires</p>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section class="highSchool" id="highSchool">
+      <h3>Lycée</h3>
+      <p>Début à 14h</p>
+      <!-- include a youtube video-->
+      <!-- include an img with the green line (general classes) ROADMAP-->
+      <p>Les ateliers partenaires</p>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <div class="workshop">
+        <div class="head">
+          <div class="title">TITLE</div>
+          <div class="image"><img src="" alt="TITLE"></div>
+        </div>
+        <div class="description">
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <!-- include an img with the green line (general classes) SATISFACTION FORM-->
+
+    <?php
+      require_once('footer.html');
+    ?>
+
+  </body>
+</html>
