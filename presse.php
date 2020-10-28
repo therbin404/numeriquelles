03fa97ccb1eb2a538cda3be48c8a48219120ba56
laurent <lauludev@hotmail.com>
2020-10-28 11:45:09 +0100

    initial commit

---
+++ presse.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_presse.css">
+    <title>Presse</title>
+</head>
+<body>
+    <?php include 'header.php'?>
+    <div class="toppage"></div>
+    <div class="titleDecoration">
+        <div class="title">
+            <div class="bar"></div>
+            <div>
+              <h1>PRESSE</h1>
+            </div>
+        </div>
+    </div>
+    <div class="presentation">
+        <p>Dans les Hauts-de-France, 3 000 emplois numériques sont non
+             pourvus chaque année. Au delà de cet enjeu économique, la parité
+              dans les métiers du numérique et de l'informatique est un enjeu
+               sociétal : en effet, comment imaginer que le monde digital de
+                demain soit conçu uniquement par des hommes, pour les hommes ?
+                 L'objectif de Numériqu'Elles est d'amener les jeunes filles à
+                  découvrir et à s’initier aux différents métiers du numérique
+                   et de l’informatique, en rencontrant des femmes travaillant
+                    dans ces milieux ainsi que des centres de formation dispensant des formations sur le territoire.
+            Ainsi, les collégiennes peuvent décrocher un stage de Troisième et les lycéennes une
+            bourse pour poursuivre leurs études en informatique (initiative portée par IBM). Depuis 2015, ce sont plus de 2000 collégiennes et lycéennes ainsi qu'une cinquantaine d'établissements qui ont pu participer à Numériqu'Elles. Pour la quatrième édition de l'événement à Lille, elles étaient près de 300 collégiennes participantes. Simultanément, à Amiens, 175 collégiennes et 160 lycéennes ont pris part à la première édition. En démultipliant Numériqu’Elles dans plusieurs sites des Hauts-de-France et d'autres régions, les actrices et acteurs publics et privé.es s'engagent durablement dans la féminisation des métiers du numérique et en faveur de l'égalité femmes/hommes</p>
+    </div>
+    <div class="blueback">
+        <a href="affiche 2020"><img src="images/Affiche Numériquelles euratechnologies 2020 V3 (003).png" alt=""></a>
+        <a href="nouvelle plaquette ONISEP.pdf"><img src="images/osez_le_num.png" alt=""></a>
+    </div>
+    <div class="archivepresse">
+        <h2>Archives</h2>
+        <a href="articlepresse.pdf"><img src="images/imgarticlepresse.png" alt=""></a>
+    </div>
+    <div class="savoir_faire">
+        <h2>Notre savoir Faire</h2>
+        <p>La première édition de Numériqu’Elles est organisée à Lille,
+             et reprise les années suivantes en associant de nouveaux
+              partenaires tout en perfectionnant l’organisation grâce notamment
+               au questionnaire de satisfaction complété par les élèves. Chaque
+                année le CORIF se charge de les dépouiller. 
+            La découverte des métiers du numérique est appréhendée par des
+             témoignages et des ateliers pratiques dans différents stands.
+            
+            Aujourd’hui 7 Numériqu’ELLES sont organisés sur le territoire des
+             HDF 20 entreprises s’engagent dans cette action et s’engagent
+              également à accepter des élèves en stage. 10 organismes de formation s’engagent aussi , Le département du Nord s’engage ainsi que les communautés d’agglomérations .</p>
+    </div>
+    <?php include 'footer.php'?>
+</body>
+</html>
