97cf494209cba6097ae56d7187bbf826b7188bdb
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-10-29 20:08:57 +0100

    added stage contact

---
+++ stage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="stage.css">
+      <title>Numériqu'elles</title>
+  </head>
+  <body>
+
+    <?php
+      require_once('header.html');
+    ?>
+    <div class="first_image">
+    </div>
+    <section class="mainContent">
+        <div class="titleDecoration observationHeader">
+          <div class="title">
+            <div class="bar"></div>
+            <div>
+              <h2>Contact</h2>
+            </div>
+          </div>
+        </div>
+        <p>Dans une volonté de découvrir de plus prêt les métiers du numérique vous souhaitez ou êtes déjà en stage dans une entreprise du numérique mais vous avez besoin de conseils ou une réponse à vos question. Madame DELROT Murielle sera répondre à vos interrogations. 
+        </p>
+        <div class="mailSection">
+            <p>Murielle DELROT</p>
+            <p>03 20 15 60 29</p>
+            <p><a href="murielle.delrot@region-academique-haut-de-france.fr" title="mail de murielle delrot">murielle.delrot@region-academique-haut-de-france.fr</a></p>
+        </div>
+    </section>
+    <?php
+      require_once('footer.html');
+    ?>
+
+  </body>
+</html>
