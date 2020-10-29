2bcccaf20c1b0d27efac6416150e9827692a1310
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-10-29 11:28:43 +0100

    added maubeuge

---
+++ maubeuge.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="styleevent.css">
+      <title>Numériqu'elles - Maubeuge</title>
+  </head>
+  <body>
+
+    <?php
+      require_once('header.html');
+    ?>
+    <!-- include a jumbotron (general classes) -->
+    <div class="event">
+      <h2>Maubeuge</h2>
+      <p>17 novembre</p>
+    </div>
+      <div class="titleDecoration roadMap">
+        <div class="title">
+            <div class="bar"></div>
+            <div>
+              <p>Découvrez les partenaires de cette édition Numériqu’elles 2020.</p>
+            </div>
+        </div>
+      </div>
+      <div class="workshops">
+        <div class="workshop">
+          <div class="head">
+            <div class="title2"><a href="https://www.gipreussir.fr/" title="Lien vers le site web RESA">RESA</a></div>
+            <div class="image">
+              <a href="https://www.gipreussir.fr/" title="Lien vers le site web RESA">
+                <img src="images/logo_resa.png" alt="RESA">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title1"><a href="https://pierre-forest-maubeuge.enthdf.fr/" title="Lien vers le site web Lycée Pierre Forest Maubeuge">Lycée Pierre Forest Maubeuge</a></div>
+            <div class="image">
+              <a href="https://pierre-forest-maubeuge.enthdf.fr/" title="Lien vers le site web Lycée Pierre Forest Maubeuge">
+                <img src="images/lycée_maubeuge_logo.jpg" alt="Lycée Pierre Forest Maubeuge">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title2"><a href="https://www.femmes-ingenieurs.org/" title="Lien vers le site web FEMMES INGENIEURS">FEMMES INGENIEURS</a></div>
+            <div class="image">
+              <a href="https://www.femmes-ingenieurs.org/" title="Lien vers le site web FEMMES INGENIEURS">
+                <img src="images/logo_fi.jpg" alt="FEMMES INGENIEURS">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title1"><a href="https://www.axa.fr/" title="Lien vers le site web AXA FRANCE">AXA FRANCE</a></div>
+            <div class="image">
+              <a href="https://www.axa.fr/" title="Lien vers le site web AXA FRANCE">
+                <img src="images/AXA_Logo.svg" alt="AXA FRANCE">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title2"><a href="https://www.intechinfo.fr/" title="Lien vers le site web INTECH SUD - MAUBEUGE">INTECH SUD - MAUBEUGE</a></div>
+            <div class="image">
+              <a href="https://www.intechinfo.fr/" title="Lien vers le site web INTECH SUD - MAUBEUGE">
+                <img src="images/logo_intech.png" alt="INTECH SUD - MAUBEUGE">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title1"><a href="https://www.orientoi.fr/" title="Lien vers le site web ORIENTOI">ORIENTOI</a></div>
+            <div class="image">
+              <a href="https://www.orientoi.fr/" title="Lien vers le site web ORIENTOI">
+                <img src="images/logo_orientoi.png" alt="ORIENTOI">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title2"><a href="https://e-catalyst.fr/" title="Lien vers le site web E-CATALYST">E-CATALYST</a></div>
+            <div class="image">
+              <a href="https://e-catalyst.fr/" title="Lien vers le site web E-CATALYST">
+                <img src="images/logo_catalyst.png" alt="E-CATALYST">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="workshop">
+          <div class="head">
+            <div class="title1"><a href="https://www.citedesgeometries.org/newcdg/" title="Lien vers le site web CITE DES GEOMETRIES">CITE DES GEOMETRIES</a></div>
+            <div class="image">
+              <a href="https://www.citedesgeometries.org/newcdg/" title="Lien vers le site web CITE DES GEOMETRIES">
+                <img src="images/logo_geometris.png" alt="CITE DES GEOMETRIES">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="titleDecoration roadMap">
+        <div class="title">
+            <div class="bar"></div>
+            <div>
+              <p>Retrouvez le programme de votre établissement</p>
+            </div>
+        </div>
+      </div>
+      <div class="program">
+        <p>Merci de veiller à ce que toutes les contraintes techniques soient prises en compte avant le jour J afin que nous puissions tous profiter de ce forum dans les meilleures conditions.<br>
+        Nous comptons sur les encadrants pour jouer le rôle de modérateurafin d’animer et réguler les échanges en collaboration avec nospartenaires sur les ateliers.<br>
+        Les jeunes filles pourront devenir « <a href="" title="Ambassadrices du numériques">Ambassadrices du numérique</a> » en participant à ce nouveau concours.</p>
+        <p>Enfin, si certaines d’entre elles souhaitent prolonger leur expériencedu numérique, elles pourront trouver un stage proposé par nos entreprises partenaires (en fonction de l’évolution de la crise sanitaire) (lien vers page stage).</p>
+        <p>Les collèges</p>
+        <ul>
+          <li><a href="ressources/middle_school_rotation_lille.docx">Télécharger la feuille de route collèges</a></li>
+        </ul>
+        <p>Les Lycées</p>
+        <ul>
+          <li><a href="ressources/high_school_rotation_lille.docx">Télécharger la feuille de route lycées</a></li>
+        </ul>
+      </div>
+    <div class="titleDecoration virtualRoom">
+      <div class="title">
+          <div class="bar"></div>
+          <div>
+            <p>Accéder au salon virtuel<br>
+            <a href="https://cvirtuelle.phm.education.gouv.fr/numeriquellesmaubeuge">https://cvirtuelle.phm.education.gouv.fr/numeriquellesmaubeuge</a>
+            </p>
+          </div>
+      </div>
+    </div>
+    <p>
+      Si vous rencontrez des difficultés à vous connecter au salon virtuel, consultez le <a href="https://drive.google.com/file/d/19W1WQsL8ZGJnUKR42gbeSfXmOqZ8fXsV/view?usp=sharing" title="PDF">PDF</a>
+    </p>
+    <p>Par mesure de sécurité, si la visioconférence VIA n’est pas fonctionnelle,
+        les partenaires ont prévu un lien Teams de secours pour proposer leur
+        atelier. <br>
+        Merci de vérifier que l’accès à Teams est possible sur le réseau de
+        votre établissement.
+    </p>
+    <div class="titleDecoration virtualRoom">
+      <div class="title">
+          <div class="bar"></div>
+          <div>
+            <p>N’oubliez pas de faire remplir le questionnaire desatisfaction par vos élèves.</p>
+          </div>
+      </div>
+    </div>
+    <p><a href="https://drive.google.com/file/d/1PFpiost2ZQtKJQVrMep0qgHCOM3Ju9aC/view?usp=sharing" title="Télécharger le PDF de satisfaction">Téléchargez le PDF de satisfaction</a>
+    </p>
+    <p>Et le retourner à l'<a href="​mdilmi@corif.fr​" title="mail Maleka">adresse mail de Maleka</a> : ​ <a href="​mdilmi@corif.fr​" title="mail Maleka">mdilmi@corif.fr​</a></p>
+
+    <!-- include an img with the green line (general classes) SATISFACTION FORM-->
+
+    <?php
+      require_once('footer.html');
+    ?>
+
+  </body>
+</html>
