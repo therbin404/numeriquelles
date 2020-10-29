55fd71b60ec9a3132702e107531ed7239d5e9122
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-10-29 23:45:51 +0100

    fix padding

--- legal_notice.php
+++ legal_notice.php
@@ -13,60 +13,60 @@
     <?php include("header.html")?>
     <div class="first_image">
     </div>
-    <div class="mainContent">
-    <div class="titleDecoration observationHeader">
-        <div class="title">
-            <div class="bar"></div>
-            <div>
-                <h1>Mentions légales</h1>
+    <section class="mainContent">
+        <div class="titleDecoration observationHeader">
+            <div class="title">
+                <div class="bar"></div>
+                <div>
+                    <h1>Mentions légales</h1>
+                </div>
             </div>
         </div>
-    </div>
-    <section id="legal_notice">
-        <h2>Mentions légales</h2>
-        <p>dane.ac-lille.fr, conformément à la loi pour la confiance dans l’économie numérique n°2004-575 du 21 juin
-            2004.
-            Editeur : Direction des Systèmes d’Information – académie de Lille – Ministère de l’Éducation Nationale,
-            de
-            l’Enseignement Supérieur, de la Recherche et de l’Innovation.</p>
+        <section id="legal_notice">
+            <h2>Mentions légales</h2>
+            <p>dane.ac-lille.fr, conformément à la loi pour la confiance dans l’économie numérique n°2004-575 du 21 juin
+                2004.
+                Editeur : Direction des Systèmes d’Information – académie de Lille – Ministère de l’Éducation Nationale,
+                de
+                l’Enseignement Supérieur, de la Recherche et de l’Innovation.</p>
 
-        <p>110 avenue Gaston Berger, 59000 Lille</p>
-        Tel: 0320956900 – dsi@ac-lille.fr
+            <p>110 avenue Gaston Berger, 59000 Lille</p>
+            Tel: 0320956900 – dsi@ac-lille.fr
 
-        <p>Directeur de publication :</p>
+            <p>Directeur de publication :</p>
 
-        ...
+            ...
 
-        <p>Rédacteur :</p>
+            <p>Rédacteur :</p>
 
-        ...
+            ...
 
-        <p>Prestataire d’hébergement : Direction des Systèmes d’Information – académie de Lille</p>
+            <p>Prestataire d’hébergement : Direction des Systèmes d’Information – académie de Lille</p>
 
-        <p>Traitement de données à caractère personnel : Conformément aux articles 13 à 22 du règlement n° 2016/679,
-            dit règlement général sur la protection des données (RGPD) du 27 avril 2016 et à l’article 32 et
-            suivants de
-            la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, vous pouvez
-            vous
-            opposer (partiellement ou totalement) à la diffusion d’informations vous concernant sur ce site.</p>
+            <p>Traitement de données à caractère personnel : Conformément aux articles 13 à 22 du règlement n° 2016/679,
+                dit règlement général sur la protection des données (RGPD) du 27 avril 2016 et à l’article 32 et
+                suivants de
+                la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, vous pouvez
+                vous
+                opposer (partiellement ou totalement) à la diffusion d’informations vous concernant sur ce site.</p>
 
-        <p>Pour exercer votre droit d’accès, de modification, de rectification, ion,
-            de limitation et de suppression de
-            données diffusées qui vous sont attachées (article 12 du règlement général
-            sur la protection des données), nous vous invitons à contacter le service suivant en
-            justifiant votre identité :</p>
+            <p>Pour exercer votre droit d’accès, de modification, de rectification, ion,
+                de limitation et de suppression de
+                données diffusées qui vous sont attachées (article 12 du règlement général
+                sur la protection des données), nous vous invitons à contacter le service suivant en
+                justifiant votre identité :</p>
 
-        <p></p>Pascale BOURBON,
-        déléguée académique à la protection des données<br> –
-        Tel: 03 20 15 67 96<br> – dpd@ac-lille.fr</p>
+            <p></p>Pascale BOURBON,
+            déléguée académique à la protection des données<br> –
+            Tel: 03 20 15 67 96<br> – dpd@ac-lille.fr</p>
 
-        Il vous est loisible d’introduire une réclamation auprès de la commission nationale informatique et libertés
-        à l’aide du lien suivant :</p>
-        <a href="https://eduline.ac-lille.fr/rgpd/cnil-plaintes">https://eduline.ac-lille.fr/rgpd/cnil-plaintes</a></p>
+            Il vous est loisible d’introduire une réclamation auprès de la commission nationale informatique et libertés
+            à l’aide du lien suivant :</p>
+            <a href="https://eduline.ac-lille.fr/rgpd/cnil-plaintes">https://eduline.ac-lille.fr/rgpd/cnil-plaintes</a></p>
 
-        </div>
+            </div>
+        </section>
     </section>
-</div>
     <?php include("footer.html")?>
 
 </body>
