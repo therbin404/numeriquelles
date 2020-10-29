a3c8bec2ca890131debdcc513c47402ed9d4108e
Sacha <urswiit@gmail.com>
2020-10-29 23:23:21 +0100

    add go further + legal notice

---
+++ to_go_further.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pour aller plus loin</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="go_further.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include("header.html")?>
+    <div class="first_image">
+    </div>
+    <div class="mainContent">
+    <div class="titleDecoration observationHeader">
+        <div class="title">
+            <div class="bar"></div>
+            <div>
+                <h1>Les ambassadrices du numérique</h1>
+            </div>
+        </div>
+    </div>
+    <section class="go_further_section1">
+        <p>Suite au forum Numériqu’elles du 8 novembre 2019,
+            il est proposé aux élèves des établissements ayant participé de réaliser une ou plusieurs productions
+            utilisant le numérique pour promouvoir ce secteur auprès des collégiennes et des lycéennes notamment.</p>
+        <p>L’objectif est de déconstruire les stéréotypes
+            liés au secteur du numérique en présentant par exemple un métier, une association,
+            un parcours personnel et/ou professionnel, une action menée dans l’établissement en lien avec le forum.</p>
+        <div class="list_center">
+            <span id="realisation_title_bold">Les réalisations possibles sont les suivantes :</span>
+            <ul>
+                <li>capsule vidéo de 3 minutes maximum,</li>
+                <li>émission d’une webradio,</li>
+                <li>page web du site de l’établissement,</li>
+                <li>diaporama,</li>
+                <li>questionnaire en ligne,</li>
+                <li>outils disponibles sur l’ENT : Madmagz, mur,</li>
+                <li>collaboratif, blog, cahier multimédia, pad.</li>
+            </ul>
+        </div>
+    </section>
+    <div id="img_realisation">
+        <img src="images/AnoukDesury-Numeriquelles2019-31.jpg" alt="filles devant pc">
+    </div>
+    <section class="go_further_section1">
+        <p>Les productions seront à déposer pour le 17 janvier 2020 dans le groupe Numériqu’elles créé sur l’ENT.
+            Un jury se réunira courant février :
+            les réalisations les plus réussies seront récompensées et serviront à la promotion du forum l’année
+            prochaine.
+        <p>
+            Les établissements qui le souhaitent peuvent être accompagnés dans ce projet par la Délégation académique au
+            numérique éducatif :<br>
+            ce.dane@ac-amiens.fr</p>
+        <p>Chaque production, réalisée en groupe qui pourra être mixte, devra être accompagnée des éléments suivants
+            (qui seront sur l’ENT) :</p>
+        <ul>
+            <li>la fiche de synthèse,</li>
+            <li>la liste des élèves,</li>
+            <li>les formulaires dûment remplis et signés
+                de cession des droits d’auteur pour tous les élèves et les adultes participant au projet,</li>
+            <li>le formulaire d’autorisation de captation de la voix et de l’image de l’élève
+                dans le cas d’une vidéo.</li>
+        </ul>
+    </section>
+    <div class="buttons">
+        <div id="button_form_pdf"><a href="ambassadrices_du_numerique.pdf">Formulaire d'inscription</a></div>
+        <div id="button_mcq_pdf"><a href="questionnaire.pdf">Questionnaire</a></div>
+    </div>
+    <div id="background_our_ambitions">
+        <div id="our_ambition">
+            <h3>Notre ambition</h3>
+            <p>Sensibiliser davantage de jeunes filles en démultipliant
+                Numériqu’Elles dans plusieurs sites des Hauts de France
+                et contribuer à l’essaimage dans d’autres régions.</p>
+        </div>
+    </div>
+</div>
+    <?php include("footer.html")?>
+</body>
+
+</html>
