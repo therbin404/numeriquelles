56196a5a8f107a01176f21d573892a08971928f2
Thomas Herbin <thomas.g.herbin@gmail.com>
2020-11-04 00:27:19 +0100

    fixed press

--- presse.php
+++ presse.php
@@ -19,39 +19,39 @@
           <div class="bar"></div>
         </div>
         <div class="presentation">
-            <p class="text">Dans les Hauts-de-France, 3 000 emplois numériques sont non
-                pourvus chaque année. Au delà de cet enjeu économique, la parité
-                dans les métiers du numérique et de l'informatique est un enjeu
-                sociétal : en effet, comment imaginer que le monde digital de
-                    demain soit conçu uniquement par des hommes, pour les hommes ?
-                    L'objectif de Numériqu'Elles est d'amener les jeunes filles à
-                    découvrir et à s’initier aux différents métiers du numérique
-                    et de l’informatique, en rencontrant des femmes travaillant
-                        dans ces milieux ainsi que des centres de formation dispensant des formations sur le territoire.
-                Ainsi, les collégiennes peuvent décrocher un stage de Troisième et les lycéennes une
-                bourse pour poursuivre leurs études en informatique (initiative portée par IBM). Depuis 2015, ce sont plus de 2000 collégiennes et lycéennes ainsi qu'une cinquantaine d'établissements qui ont pu participer à Numériqu'Elles. Pour la quatrième édition de l'événement à Lille, elles étaient près de 300 collégiennes participantes. Simultanément, à Amiens, 175 collégiennes et 160 lycéennes ont pris part à la première édition. En démultipliant Numériqu’Elles dans plusieurs sites des Hauts-de-France et d'autres régions, les actrices et acteurs publics et privé.es s'engagent durablement dans la féminisation des métiers du numérique et en faveur de l'égalité femmes/hommes</p>
+            <p class="text">Dans les Hauts-de-France, <span class="importantWord">3 000 emplois</span> numériques sont <span class="importantWord">non pourvus</span> chaque année.<br>
+                Au delà de cet enjeu économique, la <span class="importantWord">parité</span> dans les métiers du numérique et de l'informatique est un enjeu sociétal : en effet, comment imaginer que le monde digital de demain soit conçu <span class="importantWord">uniquement par des hommes</span>, pour les hommes ?</p>
+            <p class="text">L'<span class="importantWord">objectif</span> de Numériqu'Elles est d'amener les jeunes filles à <span class="importantWord">découvrir</span> et à s’initier aux différents métiers du numérique et de l’informatique, en rencontrant des <span class="importantWord">femmes</span> travaillant dans ces milieux ainsi que des centres de formation dispensant des formations sur le territoire.</p>
+            <p class="text">Ainsi, les collégiennes peuvent décrocher un stage de Troisième et les lycéennes une bourse pour poursuivre leurs <span class="importantWord">études en informatique</span> (initiative portée par IBM).</p>
+            <p class="text">Depuis 2015, ce sont plus de <span class="importantWord">2000 collégiennes et lycéennes</span> ainsi qu'une <span class="importantWord">cinquantaine d'établissements</span> qui ont pu participer à Numériqu'Elles. <br>
+                Pour la <span class="importantWord">quatrième édition</span> de l'événement à <span class="importantWord">Lille</span>, elles étaient près de <span class="importantWord">300 collégiennes participantes</span>. Simultanément, à <span class="importantWord">Amiens, 175 collégiennes et 160 lycéennes</span> ont pris part à la première édition.<br> 
+                En démultipliant Numériqu’Elles dans plusieurs sites des Hauts-de-France et d'autres régions, les actrices et acteurs publics et privé.es s'engagent durablement dans la <span class="importantWord">féminisation des métiers du numérique</span> et en faveur de l'<span class="importantWord">égalité femmes/hommes</span>.</p>
         </div>
         <div class="blueback">
-            <a href="affiche 2020"><img src="images/Affiche Numériquelles euratechnologies 2020 V3 (003).png" alt=""></a>
-            <a href="nouvelle plaquette ONISEP.pdf"><img src="images/osez_le_num.png" alt=""></a>
+            <div><p>Retrouvez l'affiche 2020 et la plaquette Onisep sur les métiers du numérique.</p></div>
+            <div>
+                <a href="ressources/affiche_2020.pdf"><img src="images/Affiche Numériquelles euratechnologies 2020 V3 (003).png" alt="Flyer 2020"></a>
+                <a href="ressources/onisep.pdf"><img src="images/osez_le_num.png" alt="Plauqette Onisep"></a>
+            </div>
         </div>
         <div class="archivepresse">
             <h2>Archives</h2>
-            <a href="articlepresse.pdf"><img src="images/imgarticlepresse.png" alt=""></a>
+            <a href="ressources/articlepresse.pdf"><img src="images/imgarticlepresse.png" alt=""></a>
         </div>
         <div class="savoir_faire">
-            <h2>Notre savoir Faire</h2>
-            <p class="text">La première édition de Numériqu’Elles est organisée à Lille,
-                et reprise les années suivantes en associant de nouveaux
-                partenaires tout en perfectionnant l’organisation grâce notamment
-                au questionnaire de satisfaction complété par les élèves. Chaque
-                    année le CORIF se charge de les dépouiller. 
-                La découverte des métiers du numérique est appréhendée par des
-                témoignages et des ateliers pratiques dans différents stands.
+            <div class="title">
+            <div class="pre"></div>
+            <div>
+                <h2>Notre savoir faire</h2>
+            </div>
+            <div class="bar"></div>
+            </div>
+            <p class="text">La première édition de Numériqu’Elles est organisée à Lille, et reprise les années suivantes en associant de nouveaux partenaires tout en perfectionnant l’organisation grâce notamment au questionnaire de satisfaction complété par les élèves.<br>
+            Chaque année le CORIF se charge de les dépouiller. <br>
+            La découverte des métiers du numérique est appréhendée par des témoignages et des ateliers pratiques dans différents stands.</p>
                 
-                Aujourd’hui 7 Numériqu’ELLES sont organisés sur le territoire des
-                HDF 20 entreprises s’engagent dans cette action et s’engagent
-                également à accepter des élèves en stage. 10 organismes de formation s’engagent aussi , Le département du Nord s’engage ainsi que les communautés d’agglomérations .</p>
+            <p class="text">Aujourd’hui <span class="importantWord">7 Numériqu’ELLES</span> sont organisés sur le territoire des HDF. <span class="importantWord">20 entreprises</span> s’engagent dans cette action et s’engagent également à accepter des élèves en <span class="importantWord">stage</span>.<br>
+            <span class="importantWord">10 organismes de formation</span>, le <span class="importantWord">département du Nord</span> ainsi que les <span class="importantWord">communautés d’agglomérations</span> s’engagent pour cet événement.</p>
         </div>
     </section>
     <?php include 'footer.html'?>
